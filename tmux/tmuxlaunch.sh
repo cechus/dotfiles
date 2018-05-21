3d11c315ed28d40e8b072d512b81bfa658adf440
cechus <cechuscsc4@gmail.com>
2018-05-20 23:55:18 -0400

    improvements in tmux automate script

--- tmux/tmuxlaunch.sh
+++ tmux/tmuxlaunch.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
+folder=$@
 SESSION=dev
 tmux new-sessio -d -s $SESSION
 tmux new-window -t $SESSION:1 -n 'local'
 
 tmux select-window -t $SESSION:1
-tmux send-keys 'cd ~/git/wwwachuchus; yarn watch' C-m
+tmux send-keys "cd $@ && yarn watch" C-m
 tmux splitw -v -p 80
-tmux send-keys 'cd ~/git/wwwachuchus' C-m
+tmux send-keys "cd $@" C-m C-l
 tmux splitw -h -p 45
-tmux send-keys 'cd ~/git/wwwachuchus; art t' C-m
+tmux send-keys "cd $@; art t" C-m C-l
 tmux splitw -v -p 30
-tmux send-keys 'cd ~/git/wwwachuchus; psql -U postgres -p 5435' C-m
+tmux send-keys "cd $folder; psql -U postgres -p 5432" C-m C-l
 
-tmux neww -t $SESSION:2 -n 'log' 'cd ~/git/wwwachuchus; lnav storage/logs/laravel.log'
+tmux neww -t $SESSION:2 -n 'log' "cd $@; lnav storage/logs/laravel.log"
 
 tmux neww -t $SESSION:3 -n 'system-monitor' 'htop'
+
 
 tmux select-window -t $SESSION:1
 
